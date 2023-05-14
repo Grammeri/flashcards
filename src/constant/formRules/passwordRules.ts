0418d220fb11dd4b691fd1d12acfe472f9c2de0b
Grammeri <89168093+Grammeri@users.noreply.github.com>
2023-05-14 10:25:49 -0500

    test commit

--- src/constant/formRules/passwordRules.ts
+++ src/constant/formRules/passwordRules.ts
@@ -1,7 +1,7 @@
 export const PASSWORD_RULES = {
     required: 'Password is required',
     minLength: {
-        message: 'Password must be more than 7 characters',
-        value: 7,
+        message: 'Password must be more than 3 characters',
+        value: 3,
     },
 };
